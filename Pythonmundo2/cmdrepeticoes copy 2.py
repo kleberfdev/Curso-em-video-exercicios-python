d7550a1f55d9adbfe684126ff0c8a05dfd97a8a2
Kleber Figueredo <kleberf.dev@gmail.com>
2022-11-13 15:26:45 -0300

    Incluindo mais 3 desafios

---
+++ Pythonmundo2/cmdrepeticoes copy 2.py
@@ -0,0 +1,5 @@
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n #que equivale a s = s + n
+print(f'O snomátorio de todos os números é igual a {s}')
---
+++ Pythonmundo2/desafio49.py
@@ -0,0 +1,3 @@
+valor = int(input('Digite o valor e descubra a sua tabuada:'))
+for c in range(1, 11):
+    print(f'{valor} x {c} = {valor*c} ')
