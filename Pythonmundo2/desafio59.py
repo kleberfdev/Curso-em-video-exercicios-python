fe1de5a41f08fafc87a885ee8da8c90859e6ea77
Kleber Figueredo <kleberf.dev@gmail.com>
2022-11-14 20:33:16 -0300

    novos desafios

---
+++ Pythonmundo2/desafio59.py
@@ -0,0 +1,32 @@
+from time import sleep
+c = 0
+res = 0
+while c != 5:
+    vlr1 = int(input('Digite o primeiro Valor: '))
+    vlr2 = int(input('Digite o segundo Valor: '))
+    print('''Digite uma das opções:
+    [1] Somar
+    [2] Multiplicar
+    [3] Maior
+    [4] novos números
+    [5] Sair do programa''')
+    c = int(input('Digite a opção: '))
+
+    if c == 1:
+        res = vlr1 + vlr2
+        print(f'A soma é de {res}')
+    elif c == 2:
+        res = vlr1 * vlr2
+        print(f'A multiplicação é de {res}')
+    elif c == 3:
+        res = max(vlr1, vlr2)
+        print(f'O maior valor é {res}')
+    elif c == 4:
+        print('Aguarde, ', end='')    
+    if c == 5:
+        print('Programa encerrado')
+    else:
+        print('Vamos recomeçar')
+        sleep(2)
+        print("-"*20)
+        sleep(2)
