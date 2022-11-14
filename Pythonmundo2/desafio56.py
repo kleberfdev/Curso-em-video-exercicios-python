56de395c67a30743dfa4609b859992acb4d4bedb
Kleber Figueredo <kleberf.dev@gmail.com>
2022-11-14 00:01:18 -0300

    .

---
+++ Pythonmundo2/desafio56.py
@@ -0,0 +1,5 @@
+for c in range(0, 4):
+    nome = str(input('Digite o {}° nome: '))
+    idade = int(input('Digite a {}° idade'))
+    sexo = str(input('Qual o sexo do {}°: "M" ou "F": '))
+    
--- Pythonmundo2/teste.py
+++ Pythonmundo2/teste.py
@@ -1,5 +1,3 @@
-frase = 'kleber'
-print(frase[::-1])
-#1 onde a frase começa literalmente não importa se está ao contrário
-#2 onde a frase termina - 0 é o inicio ou fim e sem valor pega a frase toda
-#3  1 diz da esquerda para direita e -1 o inverso
+dicio = {'chave': 'valor'}
+
+print(type(dicio))
