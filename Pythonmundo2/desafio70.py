e1ea84e8c0ef87c9dda0b6cd00688a8a6f0eee70
Kleber Figueredo <kleberf.dev@gmail.com>
2023-07-06 18:26:13 -0300

    novos desafios

--- Pythonmundo2/desafio70.py
+++ Pythonmundo2/desafio70.py
@@ -1,39 +1,20 @@
-class Car():
-    def _init_(self):
-        pass
-
-    def imprimirAtributosCarro(self):
-        self.modelo = input('Digite o modelo do seu carro: ')
-        self.marca = input('Digite a marca do seu carro: ')
-        self.preco = input('Digite o preço do seu carro: ')
-        self.cor = input('Digite a cor do seu carro: ')
-        print(
-            f'Bem vindo(a)!\nO modelo do seu carro é {self.modelo}, da marca {self.marca}\nNa cor {self.cor}, ele se encontra na faixa de preço de R${self.preco},00')
-
-    def atendimentoCarro(self):
-        self.nomeCliente = input('Digite seu nome: ')
-        self.pergunta = input(f'Olá, {self.nomeCliente}! Em quê posso ajudá-lo?\nDigite 1 para a troca de óleo\nDigite 2 para reabastecer\nDigite 3 para verificação rotineira\n')
-        if(self.pergunta == '1'):
-            valorOleo = float(50.50)
-            print(f'Para trocar o óleo, fica no valor de R${valorOleo}')
-        elif(self.pergunta == '2'):
-            gasolina = float(5.50)
-            diesel = float(6.10)
-            self.abastecimento = input('Digite 1 para gasolina, ou digite 2 para Diesel ')
-            if(self.abastecimento == '1'):
-                self.litrosGasolina = input('Quantos litros você quer? ')
-                self.precoDoAbastecimentoComGasolina = gasolina * float(self.litrosGasolina)
-                print(f'Encher o motor com a gasolina vai custar {self.precoDoAbastecimento}')
-            else:
-                self.litrosDiesel = input('Quantos litros você quer? ')
-                self.precoDoAbastecimentoComDiesel = diesel * float(self.litrosDiesel)
-                print(f'Encher o motor com diesel vai te custar {self.precoDoAbastecimentoComDiesel}')
-        elif(self.pergunta == '3'):
-            valorVerificacao = float(80.90)
-            print(f'Para verificar, vai custar {valorVerificacao}')
-        else:
-            print('Desculpe, não foi uma opção válida. Peço que tente novamente.')
-            
-hilux = Car()
-hilux.imprimirAtributosCarro()
-hilux.atendimentoCarro()
+total = totmil = cont = menor = 0
+prodbarato = " "
+while True:
+    produto = str(input("Qual o Nome do Produto? "))
+    preço = float(input("Qual o Preço? R$ "))
+    cont += 1        
+    total += preço
+    if cont == 1 or preço < menor:
+        menor = preço
+    if preço > 1000:
+        totmil +=1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('{:-^40}'.format(' FIM DO PROGRAMA '))
+print(f'O total da compra foi {total:.2f}')
+print(f'O total de itens acima de 1000 Reais é {total:.2f}')
+print(f'O Produto com menor preço é {prodbarato} com valor de {menor:.2f}')
---
+++ meus testes/pythonmundo3/tuplas.py
@@ -0,0 +1,7 @@
+lanche = ["a", "b", "c", "d", "e"]
+print(lanche[2])
+print(lanche[1:])
+print(lanche[-1])
+print(len(lanche))
+for c in lanche:
+    print(c)
