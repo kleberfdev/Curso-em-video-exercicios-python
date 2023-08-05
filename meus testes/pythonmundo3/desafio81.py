c5bbcb5d6bf0e5abfb75d38b2499b1d501eabee2
Kleber Figueredo <kleberf.dev@gmail.com>
2023-08-05 15:39:05 -0300

    Update desafio81.py

---
+++ meus testes/pythonmundo3/desafio81.py
@@ -0,0 +1,10 @@
+from random import choice
+
+# Crie uma lista de frutas
+frutas = ["maçã", "banana", "laranja", "uva", "melancia"]
+
+# Use a função choice para selecionar aleatoriamente uma fruta da lista
+fruta_aleatoria = choice(frutas)
+
+# Imprima a fruta escolhida aleatoriamente
+print("Fruta escolhida aleatoriamente:", fruta_aleatoria)
